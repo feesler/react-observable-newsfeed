556ef4a8e90fcc37a2cb93c09c86ca98d8b504a4
Ilya Makarov <feesler@gmail.com>
2021-03-31 15:33:34 +0300

    + Add support of photo at link attachment

--- frontend/src/components/LinkAttachment.jsx
+++ frontend/src/components/LinkAttachment.jsx
@@ -4,11 +4,27 @@ import PropTypes from 'prop-types';
 function LinkAttachment(props) {
   const { link } = props;
 
+  const imageContent = (link.photo)
+    ? (
+      <div className="post-attachment__image">
+        <picture>
+          {link.photo.sizes.map((item) =>
+            <source key={item.type} src={item.url} />
+          )}
+          <img className="post-attachment__photo" src={link.photo.sizes[0].url} />
+        </picture>
+      </div>
+    )
+    : null;
+
   return (
     <a className="post-attachment link-attachment" href={link.url}>
-      <h2 className="post-attachment__title">{link.title}</h2>
-      <span className="post-attachment__caption">{link.caption}</span>
-    </a>
+      {imageContent}
+      <div className="post-attachment__content">
+        <h2 className="post-attachment__title">{link.title}</h2>
+        <span className="post-attachment__caption">{link.caption}</span>
+      </div>
+    </a >
   );
 }
 
