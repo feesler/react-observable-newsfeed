56860f9746102d214e767b763028a5f7be1e2938
Ilya Makarov <feesler@gmail.com>
2021-03-31 03:27:06 +0300

    + Add support of link attachments

--- frontend/src/components/LinkAttachment.jsx
+++ frontend/src/components/LinkAttachment.jsx
@@ -5,15 +5,25 @@ function LinkAttachment(props) {
   const { link } = props;
 
   return (
-    <div>
-
-    </div>
+    <a className="post-attachment link-attachment" href={link.url}>
+      <h2 className="post-attachment__title">{link.title}</h2>
+      <span className="post-attachment__caption">{link.caption}</span>
+    </a>
   );
 }
 
 LinkAttachment.propTypes = {
+  link: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    caption: PropTypes.string,
+  }).isRequired,
+};
 
+LinkAttachment.defaultProps = {
+  link: {
+    caption: '',
+  },
 };
 
 export default LinkAttachment;
-
--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -6,24 +6,35 @@ import { ReactComponent as LikesIcon } from '../assets/likes-icon.svg';
 import { ReactComponent as CommentsIcon } from '../assets/comments-icon.svg';
 import { ReactComponent as RepostsIcon } from '../assets/reposts-icon.svg';
 import { ReactComponent as ViewsIcon } from '../assets/views-icon.svg';
+import LinkAttachment from './LinkAttachment';
 import IconCounter from './IconCounter';
 
 function NewsCard(props) {
   const { item } = props;
-  const postTime = item.date * 1000;
+  const userName = 'User name';
+  const timeFmt = formatTimePretty(item.date * 1000);
 
   const decodedContent = decodeXML(item.text);
+
+  let linkAttachment = null;
+  if (Array.isArray(item.attachments)) {
+    const attach = item.attachments.find((attachment) => attachment.type === 'link');
+    if (attach) {
+      linkAttachment = <LinkAttachment link={attach.link} />;
+    }
+  }
 
   return (
     <div className="feed-item post">
       <div className="post__header">
         <div className="post__avatar"></div>
         <div className="post__title">
-          <div className="post__user">User name</div>
-          <div className="post__time">{formatTimePretty(postTime)}</div>
+          <div className="post__user">{userName}</div>
+          <div className="post__time">{timeFmt}</div>
         </div>
       </div>
       <p className="post__content">{decodedContent}</p>
+      {linkAttachment}
       <div className="post__bottom">
         <IconCounter icon={LikesIcon} count={item.likes.count} />
         <IconCounter icon={CommentsIcon} count={item.comments.count} />
